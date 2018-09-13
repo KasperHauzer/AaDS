a8282915ea6fd346501c5ebbe2867b5b95325bfd
Arsanukaev <arsanukaev@MacBook-Pro-Arsanukaev.local>
2018-09-13 16:58:52 +0500

    First lab completed

---
+++ FirstLab/Model.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FirstLab
+{
+    /// <summary>
+    /// Данный класс содержит конструктивную часть лабораторной работы №1. 
+    /// </summary>
+    public static class Model
+    {
+        /// <summary>
+        /// Оповещает о том, что стек вызовов был переполнен, описание передается как первый аргумент.
+        /// </summary>
+        public static event Action<Object, EventArgs> DetectedDangerNumber;
+
+        /// <summary>
+        /// Находим x(n)-й элемент при помощи функции.
+        /// </summary>
+        /// <returns>x(n)-й элемент.</returns>
+        /// <param name="n">Порядковый номер искомого элемента (начинается с 0).</param>
+        public static (Int32 answer, TimeSpan delay, Boolean good) SolveFunctionaly(Int32 n)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            Int32 answer = (9 * n - 4) * (Int32)Math.Pow(-1, n) + 3 * n + 3;
+            stopwatch.Stop();
+
+            return (answer, stopwatch.Elapsed, true);
+        }
+
+        /// <summary>
+        /// Находит x(n)-ый элемент итерационно.
+        /// </summary>
+        /// <returns>x(n)-ый элемент.</returns>
+        /// <param name="n">Порядковый номер искомого элемента (начинается с 0).</param>
+        public static (Int32 answer, TimeSpan delay, Boolean good) SolveIteratively(Int32 n)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            Int32 zero = -1;
+            Int32 first = 1;
+            Int32 current = default(Int32);
+
+            for (Int32 i = 2; i <= n; i++) {
+                current = 12 * i - 2 * first - zero;
+                zero = first;
+                first = current;
+            }
+            stopwatch.Stop();
+
+            if (n == 0) return (zero, stopwatch.Elapsed, true);
+            if (n == 1) return (first, stopwatch.Elapsed, true);
+            return (current, stopwatch.Elapsed, true);
+        }
+
+        /// <summary>
+        /// Находит x(n)-ый элемент рекурсивно.
+        /// </summary>
+        /// <returns>x(n)-ый элемент.</returns>
+        /// <param name="n">Порядковый номер искомого элемента (начинается с 0).</param>
+        public static (Int32 answer, TimeSpan delay, Boolean good) SolveRecursively(Int32 n)
+        {
+            Int32 maxN = 5500;
+            if (n >= maxN) {
+                OnDetectedDangerNumber("Рекурсивный подсчет был отменен из-за вероятности " +
+                                       $"\n\tпереполнения стека вызовов (n >= {maxN}).");
+                return default((Int32, TimeSpan, Boolean));
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            Int32 answer = default(Int32);
+
+            stopwatch.Start();
+            answer = RunRecursion(2, n, 0, 1, -1);
+            stopwatch.Stop();
+
+            return (answer, stopwatch.Elapsed, true);
+
+        }
+
+        /// <summary>
+        /// Рекурсивно подсчитывает x(n)-й эемент.
+        /// </summary>
+        /// <returns>x(n)-й эемент.</returns>
+        /// <param name="i">Номер текущего подсчитываемого элемента.</param>
+        /// <param name="number">Номер требуемого элемента.</param>
+        /// <param name="current">Текущий элемент.</param>
+        /// <param name="previous">Предыдущий элемент.</param>
+        /// <param name="superPrevious">Очень предыдущий элемент (дважды предыдущий).</param>
+        static Int32 RunRecursion(Int32 i, Int32 number, Int32 current, Int32 previous, Int32 superPrevious)
+        {
+            if (number == 0) return superPrevious;
+            if (number == 1) return previous;
+
+                current = 12 * i - 2 * previous - superPrevious;
+            return i == number ?
+                current :
+                RunRecursion(++i, number, 0, current, previous);
+        }
+
+        /// <summary>
+        /// Проверяет число подписчиков события, и если есть хотя бы 1 подписчик
+        /// вызывает подписанный на событие метод.
+        /// </summary>
+        static void OnDetectedDangerNumber(String description)
+        {
+            DetectedDangerNumber?.Invoke(description, new EventArgs());
+        }
+    }
+}
--- FirstLab/Program.cs
+++ FirstLab/Program.cs
@@ -1,12 +1,95 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace FirstLab
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Лабораторная работа №1";
+
+            //Подписываем метод на событие "Обнаружено опасное число" класса Model,
+            //так как рекурсивный метод переполняет стек вызовов.
+            Model.DetectedDangerNumber += ShowWarning;
+
+            do {
+                //Просим пользователя ввести число до тех пор, пока он не введет целое число
+                //больше -1. Переменную "n" передаем в методы для подсчета x(n)-го элемента 
+                //последовательности. 
+                Int32 n = 0;
+                Console.Write("Введите целое число: ");
+                while (!Int32.TryParse(Console.ReadLine(), out n) || n < 0)
+                    Console.Write("Введите целое число больше -1: ");
+
+                //Сообщаем пользователю о том, что процес поддсчета x(n)-го элемента начат, 
+                //и меняем цвет шрифта для выделения результатов.
+                Console.WriteLine("\n<Процесс начат>");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+
+                //Подсчет функцией.
+                Task solveByFunc = Task.Factory.StartNew(delegate
+                {
+                    var a = Model.SolveFunctionaly(n);
+                    ShowResult("Функция", a);
+                });
+
+                //Подсчет в цикле.
+                Task solveByIteration = Task.Factory.StartNew(delegate
+                {
+                    var a = Model.SolveIteratively(n);
+                    ShowResult("Цикл", a);
+                });
+
+                //Подсчет рекурсивно.
+                Task solveByRecursion = Task.Factory.StartNew(delegate
+                {
+                    var a = Model.SolveRecursively(n);
+                    ShowResult("Рекурсия", a);
+                });
+
+                //На данном этапе мы должно дождаться выполнения всех трех фоновых потоков,
+                //для того, чтобы после их заверщшения сообщить пользователю о том, что подсчет
+                //завершен. 
+                solveByFunc.Wait();
+                solveByIteration.Wait();
+                solveByRecursion.Wait();
+
+                //Меняем цвет консоли на стандартный и сообщает пользователю о том, что подсчет
+                //x(n)-го элемента завершен.
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("\n<\\Процесс завершен>\n");
+
+                //Даный цикл выполняется бесконечно для того, чтобы программа не прекращала свою
+                //работу до тех пор, пока пользователь не закрое ее самостоятельно. 
+            } while (true);
+        }
+
+        /// <summary>
+        /// Показывает результат работы методов для подсчета x(n)-го элемента последовательности.
+        /// </summary>
+        /// <param name="config">Метод для подсчета x(n)-го элемента последовательности.</param>
+        /// <param name="typle">Картеж, который содержит ответ и интервал времени, который затратил метод на подсчет ответа.</param>
+        static void ShowResult(String config, (Int32 answer, TimeSpan delay, Boolean good) typle)
+        {
+            if (!typle.good) return;
+
+            //Если задержка меньше милисекунды, то выводим число тактов, иначе милисекунды.
+            String time = typle.delay.Milliseconds < 1 ?
+                               typle.delay.Ticks + " тк" :
+                               typle.delay.Milliseconds + " мс";
+
+            //Составляем строку из входных параметров для вывода на консоль и выводим ее.
+            String showingString = $"\n\t{config}.\n\tОтвет: {typle.answer}\n\tЗадержка: {time}";
+            Console.WriteLine(showingString);
+        }
+
+        /// <summary>
+        /// Выводит сообщение о том, что входные данные могу повлечь переполнение стека.
+        /// </summary>
+        static void ShowWarning(Object source, EventArgs e)
+        {
+            Console.WriteLine($"\n\t{source}");
         }
     }
 }
